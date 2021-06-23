83c11dde67752219c8f7b9943734a65b559c9e87
monica Arbelaez <moniarbe233@gmail.com>
2021-06-22 20:59:31 -0500

    se hace el primer test mockTest

---
+++ src/EjercicioJest/App.test.js
@@ -0,0 +1,20 @@
+import * as app from "./app";
+import * as math from "./math";
+
+math.multiply = jest.fn();
+math.add = jest.fn();
+math.subtract = jest.fn();
+
+test("calls math.add", () => {
+  app.doAdd(1, 2);
+  expect(math.add).toHaveBeenCalledWith(1, 2);
+});
+
+test("calls math.subtract", () => {
+  app.doSubtract(1, 2);
+  expect(math.subtract).toHaveBeenCalledWith(1, 2);
+});
+test("call math.multiply",()=>{
+    app.doMultiply(1,2);
+    expect(math.multiply).toHaveBeenCalledWith(1,2);
+})
