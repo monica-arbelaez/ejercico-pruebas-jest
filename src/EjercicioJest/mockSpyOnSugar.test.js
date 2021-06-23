832f2d318535f1fed3795d38ccee89eee4c595ed
monica Arbelaez <moniarbe233@gmail.com>
2021-06-22 23:28:40 -0500

    Se hace la despripcion de la funcion del mockSpyOnSugar

--- src/EjercicioJest/mockSpyOnSugar.test.js
+++ src/EjercicioJest/mockSpyOnSugar.test.js
@@ -1,6 +1,16 @@
 import * as app from "./app";
 import * as math from "./math";
 
+/*Descripción: Inicialmente se reasigna una función 
+(en este caso math.add) a la función del mock, con el 
+objetivo de que se pueda llamar al mock en lugar de la 
+función original en cualquier lugar en donde se vaya a usar, 
+luego se “espia” el llamado de la función, dejando la 
+implementación original sin afectar (se observa la 
+  comprobación de la función sin ser reemplazada con un mock)
+Además se simula la función para mockear su funcionamiento 
+(sobrescribiendo), para luego poder restaurar la implementación original.
+*/
 test("calls math.add", () => {
   // store the original implementation
   const originalAdd = math.add;
